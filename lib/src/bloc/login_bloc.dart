fd1512b663bd104f90bdf0e8a222259c80f061ee
Daniel Ojeda <ing.danniel@outlook.com>
2019-08-21 17:29:51 -0500

    Se agregan InheritedWidget y Stream para formulario

---
+++ lib/src/bloc/login_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+class LoginBloc{
+
+
+  final _emailController    = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
+
+  //Recupera los datos del stream
+  Stream<String> get emailStream    => _emailController.stream;
+  Stream<String> get passwordStream => _passwordController.stream;
+
+  //Insertar valores al Stream
+
+  Function(String) get changeEmail    => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  //cerrar Stream
+  dispose(){
+    _emailController?.close();
+    _passwordController?.close();
+  }
+
+
+}
